d58edbd918bf0ecc965e55941e4c573258e27dd7
hosamation <mohammad.hosam@gmail.com>
2022-06-20 23:00:38 +0200

    Finish JS

--- 09 - Dev Tools Tricks Domination/main.js
+++ 09 - Dev Tools Tricks Domination/main.js
@@ -7,25 +7,63 @@ function makeGreen() {
 }
 
 // Regular
+console.log("Hello");
 
 // Interpolated
+console.log("Hello I am a %s string", "💩");
 
 // Styled
+console.log("%c I am some great text", "font-size: 30px; background: red; text-shadow: 5px 5px 0 blue");
 
 // warning!
-
+console.warn("OH Hooo");
 // Error :|
-
+console.error("Shit!");
 // Info
+console.log("Crocodiles eat 3-4 people per year");
 
 // Testing
-
+const p = document.querySelector("p");
+console.assert(p.classList.contains("ouch"), "That is Wrong!");
 // clearing
+// console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);
 
 // Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} year old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count("Mohammad");
+console.count("Mohammad");
+console.count("Hosam");
+console.count("Hosam");
+console.count("Mohammad");
+console.count("Hosam");
+console.count("Hosam");
+console.count("Hosam");
+console.count("Hosam");
+console.count("Hosam");
+console.count("Mohammad");
+console.count("Mohammad");
+console.count("Mohammad");
 
 // timing
+console.time("Fetching Data");
+fetch("https://api.github.com/users/hosamation")
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd("Fetching Data");
+    console.log(data);
+});
+
+// Table
+console.table(dogs);
